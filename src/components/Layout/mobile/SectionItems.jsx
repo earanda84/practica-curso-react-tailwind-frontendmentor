5861beaaadcaa4736156c858f82ae6331a85ebbc
earanda84 <eaamoran@gmail.com>
2023-12-22 23:48:25 -0300

    change padding on SectionItem components

--- src/components/Layout/mobile/SectionItems.jsx
+++ src/components/Layout/mobile/SectionItems.jsx
@@ -4,7 +4,7 @@ import Button from "./Button"
 const SectionItems = () => {
   return (
     <section className="container mx-auto px-5 mb-3">
-        <div className="flex justify-center gap-4 bg-white rounded-md p-4 mx-1">
+        <div className="flex justify-center gap-4 bg-white rounded-md p-4">
             <Button className="text-blue-600" label="All" />
             <Button className="hover:text-blue-600" label="Active" />
             <Button className="hover:text-blue-600" label="Completed" />
